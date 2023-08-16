b47e3f8c637f50405ea146c2fe5caa46aa714bec
isolateminds <isolateminds@gmail.com>
2023-08-16 18:13:12 -0400

    remove complexities

--- cmd/print.go
+++ cmd/print.go
@@ -3,23 +3,15 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/ttacon/chalk"
 )
 
-// Prints a error message and calls a optional cleanup function and then os.Exit(1)
-func FatalError(from string, err error, cleanupFns ...func() error) {
-	msg := strings.ReplaceAll(err.Error(), ":", " -> ")
-	fmt.Printf("(%s): %s\n", chalk.Red.Color(from), chalk.White.Color(msg))
-	for _, cleanup := range cleanupFns {
-		err := cleanup()
-		if err != nil {
-			fmt.Printf("(%s): %s\n", chalk.Red.Color("CleanupError"), chalk.White.Color(msg))
-		}
-	}
+// Prints a error message and then calls os.Exit(1)
+func PrintFatalError(err error) {
+	fmt.Println(chalk.White.Color(err.Error()))
 	os.Exit(1)
 }
-func Success(msg string) {
+func PrintSuccess(msg string) {
 	fmt.Println(chalk.Green.Color(msg))
 }
